4ccd9e4f0917232876731316403781f0868c2ef8
notmike <notmike-5@users.noreply.github.com>
2025-09-18 20:22:02 -0600

    rpc.py: updated calling conventions and added some more intuitive error-handling

--- rpc.py
+++ rpc.py
@@ -3,7 +3,7 @@ import os
 import requests
 
 def _get_cookie(cookiefile: str) -> (str, str):
-    '''Obtain the cookie for cookie auth (default)'''
+    '''Obtain the cookie user/pass for cookie authentication (default)'''
     if not os.path.exists(cookiefile):
         raise FileNotFoundError(
             f"Cookie not found at {cookiefile}. Ensure bitcoind is running and datadir is correct.")
@@ -20,8 +20,10 @@ def make_rpc_call(method: str = "getblockchaininfo",
                   user: str = None,
                   password: str = None,
                   datadir: str ="~/.bitcoin",
-                  rpc_url: str = "http://127.0.0.1:8332"):
+                  rpc_ip: str = "127.0.0.1",
+                  rpc_port: int = 8332) -> int | dict
     '''Handle rpc calls to the bitcoind'''
+
     headers = {'content-type': 'application/json'}
     payload = {
         "jsonrpc": "2.0",
@@ -30,24 +32,47 @@ def make_rpc_call(method: str = "getblockchaininfo",
         "params": params or []
     }
 
-    if user is not None and password is not None:
-        auth = (user, password)
-    else:
-        if datadir == '~/.bitcoin':
+    # get auth credentials
+    match (user, password):
+        case (None, None):  # default to cookie authentication
             datadir = os.path.expanduser(datadir)
-        cookie_file = os.path.join(datadir, '.cookie')
-        auth = _get_cookie(cookie_file)
+            cookie_file = os.path.join(datadir, '.cookie')
+            auth = _get_cookie(cookie_file)
+        case (None, _):  # error case: no user given
+            raise ValueError("rpcuser is required w/ rpcpassword")
+        case (_, None):  # error case: no password given
+            raise ValueError("rpcpassword is required w/ rpcuser")
+        case (_, _): # roll with what the user gave us...
+            auth = (user, password)
 
+    # make the request
     try:
         response = requests.post(
-            rpc_url,
+            f"http://{rpc_ip}:{rpc_port}",
             auth=auth,
             data=json.dumps(payload),
             headers=headers,
             timeout = 1.5
         )
-        response.raise_for_status()  # Raise error on bad HTTP status
+        response.raise_for_status()  # error on bad HTTP status
+
         return response.json().get('result')
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
+
         return None
+
+# these currently assume you are using cookie auth
+
+def getrawtransaction(txid: str,
+                      verbosity: int=None,
+                      blockhash=None,
+                      datadir="~/.bitcoin"):
+    '''get transaction from the bitcoind'''
+    params = [txid]
+    if isinstance(verbosity, int):
+        params.append(verbosity)
+    if isinstance(blockhash, str):
+        params.append(blockhash)
+
+    return make_rpc_call(method='getrawtransaction', params=params, datadir=datadir)
